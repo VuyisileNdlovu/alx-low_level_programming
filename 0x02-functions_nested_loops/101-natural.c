4559e5a80735301cd6c744eecfaeb12de20d2dc1
VuyisileNdlovu <vuyisilendlovu44@gmainl.com>
2023-02-22 16:41:53 -0800

    Task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,16 +5,14 @@
  * Return: Always (Success)
  */
 
-int main (void)
-
+int main(void)
 {
-	int i z = 0;
-
+int i, z = 0;
 	while (i < 1024)
 	{
 		if ((i % 3 == 0) || (i % 5 == 0))
 		{
-			z +=i;
+			z += i;
 		}
 		i++;
 	}
