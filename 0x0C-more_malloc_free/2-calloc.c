27db20f02e06e48dc5dca25e306ae983ad449a8d
VuyisileNdlovu <vuyisilendlovu44@gmainl.com>
2023-03-16 09:59:17 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -6,18 +6,18 @@
  * @s: memory area to be filled
  * @b: char to copy
  * @n: number of times to copy b
- * 
+ *
  * Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
-	
+
 	return (s);
 }
 
@@ -31,16 +31,16 @@ char *_memset(char *s, char b, unsigned int n)
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
-	
+
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	
+
 	ptr = malloc(size * nmemb);
-	
+
 	if (ptr == NULL)
 		return (NULL);
-	
+
 	_memset(ptr, 0, nmemb * size);
-	
+
 	return (ptr);
 }
