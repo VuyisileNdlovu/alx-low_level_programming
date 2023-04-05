f693cd4af6dc340a234934a4428df445e771f4d3
VuyisileNdlovu <vuyisilendlovu44@gmail.com>
2023-04-05 12:21:56 -0700

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
-* add_nodeint - adds a new node at the beginning of a linked list
+* add_nodeint - This code implements the function that adds a new node at the beginning of a linked list
 * @head: pointer to the first node in the list
 * @n: data to insert in that new node
+* Author Vuyisile Ndlovu
 *
 * Return: pointer to the new node, or NULL if it fails
 */
