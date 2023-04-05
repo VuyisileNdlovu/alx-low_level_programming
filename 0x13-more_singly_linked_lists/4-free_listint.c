da0a31cb9b8d7f0f7c7f21ee4582d6f35f37954e
VuyisileNdlovu <vuyisilendlovu44@gmail.com>
2023-04-05 13:35:19 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,17 +1,23 @@
 #include "lists.h"
 
 /**
-* free_listint - frees a linked list
+* free_listint - function frees a linked list
 * @head: listint_t list to be freed
+*
+* This function frees a linked list of integer
+* It takes a pointer to the head of the list as its argument,
+* and frees each node in the  list using the free function.
+*
+* Return: None
 */
 void free_listint(listint_t *head)
 {
-listint_t *temp;
+	listint_t *temp;
 
-while (head)
-{
-temp = head->next;
-free(head);
-head = temp;
-}
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
 }
